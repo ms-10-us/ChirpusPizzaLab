183f82234f32a51d64c2a681181e1f36ce829919
Mazen Saber <mazensaber1996@gmail.com>
2025-02-02 09:23:58 -0500

    Chirpus Pizza Lab - Mazen Saber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './interface/menu-item';
+import { mapToCanActivate } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChripusPizzaLab';
+
+  menuItem: MenuItem[] = [
+    {name: 'Chicken Fingers', catagory: 'Dinner', price: 11.99, vegetarian: false, soldOut: false},
+    {name: 'Pizza', catagory: 'Dinner', price: 11.99, vegetarian: true, soldOut: false},
+    {name: 'Pepperoni Pizza', catagory: 'Dinner', price: 12.99, vegetarian: false, soldOut: false},
+    {name: 'Bread Sticks', catagory: 'Sides', price: 4.99, vegetarian: true, soldOut: true},
+    {name: 'Wings', catagory: 'Sides', price: 8.99, vegetarian: false, soldOut: false},
+    {name: 'Cinnamon Roll', catagory: 'Dessert', price: 8.99, vegetarian: true, soldOut: false},
+  ]
+
+  totalPrice: number = 0;
+
+  addToOrder(menuItem: MenuItem): void {
+    this.totalPrice += menuItem.price;
+  }
+
 }
---
+++ src/app/interface/menu-item.ts
@@ -0,0 +1,7 @@
+export interface MenuItem {
+    name: string,
+    catagory: string,
+    price: number,
+    vegetarian: boolean,
+    soldOut: boolean
+}
